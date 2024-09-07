d6eb6a88c0cfaa828c7b41a891ad5c59167308b8
webgaurav02 <gauravcodes123@gmail.com>
2024-09-07 15:11:33 +0530

    Fix og-image, twitter-image and add footer links

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -10,10 +10,10 @@ const Footer = () => {
   return (
     <div className=' mt-40 sm:mt-0'>
       <div className='flex justify-center mt-11 gap-3 '>
-        <Link href=""><FaFacebookSquare className='text-4xl text-white' /></Link>
-        <Link href=""><FaInstagramSquare className='text-4xl text-white' /></Link>
-        <Link href=""> <FaLinkedin className='text-4xl text-white' /></Link>
-        <Link href=""><FaSquareWhatsapp className='text-4xl text-white' /></Link>
+        <Link href="https://www.facebook.com/p/The-Yeastern-Civilization-61554142565935/" target='_blank' rel='norefferer noopener'><FaFacebookSquare className='text-4xl text-white' /></Link>
+        <Link href="https://www.instagram.com/tycshillong/" target='_blank' rel='norefferer noopener'><FaInstagramSquare className='text-4xl text-white' /></Link>
+        <Link href="https://www.instagram.com/reel/C4GAauata8r/" target='_blank' rel='norefferer noopener'> <FaLinkedin className='text-4xl text-white' /></Link>
+        <Link href="https://api.whatsapp.com/send/?phone=916909378111&text=Reservation&type=phone_number&app_absent=0" target='_blank' rel='norefferer noopener'><FaSquareWhatsapp className='text-4xl text-white' /></Link>
       </div>
       <div className="mt-2">
         <div className="border-t border-white border-[1px]"></div>
--- src/app/layout.js
+++ src/app/layout.js
@@ -8,6 +8,35 @@ import Navbar from '@/app/components/navbar'
 export const metadata = {
   title: "The Yeastern Civilization - Shillong's First Microbrewery & Restaurant",
   description: "Discover The Yeastern Civilization, Shillong's first microbrewery offering fresh, locally brewed craft beer. Experience the unique blend of local flavors and cultural heritage in a cozy setting, perfect for locals and tourists alike.",
+  canonical: '/',
+  languages: {
+    'en-US': '/en-US',
+  },
+  openGraph: {
+    type: 'website',
+    url: 'https://tycindia.com',  // replace with your domain
+    title: "The Yeastern Civilization - Shillong's First Microbrewery & Restaurant",
+    description: "Discover The Yeastern Civilization, Shillong's first microbrewery offering fresh, locally brewed craft beer.",
+    images: [
+      {
+        url: 'https://tycindia.com/opengraph-image.jpg',  // full URL of the OG image
+        width: 1200,
+        height: 630,
+        alt: 'The Yeastern Civilization Banner',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',  // Specifies the type of card (summary or summary_large_image)
+    title: "The Yeastern Civilization - Shillong's First Microbrewery & Restaurant",
+    description: "Discover The Yeastern Civilization, Shillong's first microbrewery offering fresh, locally brewed craft beer.",
+    images: [
+      {
+        url: 'https://tycindia.com/opengraph-image.jpg',  // Same OG image for Twitter
+        alt: 'The Yeastern Civilization Banner',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
